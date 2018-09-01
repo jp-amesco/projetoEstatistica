08f8f873a52cd5285fd4d32872daff413cdd3b95
João <joaopauloamesco@gmail.com>
2018-09-01 12:52:10 -0300

    ajustando testes para ficar igual a entrada do usuario

--- testes/entradaDados/entradaDadosTestes.js
+++ testes/entradaDados/entradaDadosTestes.js
@@ -4,7 +4,7 @@ const entradaDados = require('../../js/dados/entradaDados.js');
 //mensagem que aparecerá no console em caso de erro
 it('espera-se que a função retorne um array', function(){
 	//dados enviados para testar a função
-	dados = [1, 2, 4, 5];
+	dados = '1, 2, 4, 5';
 	//recebe a resposta da função
 	array = entradaDados.init(dados);
 	//retorno da função, espera-se true
@@ -22,7 +22,7 @@ it('espera-se que a função retorne mensagem caso os dados recebidos seja nulo'
 //mensagem que aparecerá no console em caso de erro
 it('espera-se que a função retorne mesagem de erro caso os dados recebidos seja inválidos', function(){
 	//dados enviados para testar a função
-	dados = ['A', 1, 5, '2', 125];
+	dados = 'A, 1, 5, 2, 125';
 	//recebe a resposta da função
 	array = entradaDados.init(dados);
 	//retorno da função, espera-se true
--- testes/moda/modaQualitativa.js
+++ testes/moda/modaQualitativa.js
@@ -2,13 +2,13 @@
 const modaQualitativa = require('../../js/moda/modaQualitativa');
 
 it('Espera-se que a função calcule a moda recebendo a variavel qualitativa', function(){
-    dados = [
-    		 'EF', 'EF', 'EF', 'EM', 'EM',
-    		 'EM', 'EM', 'EM', 'EM', 'EM', 
-    		 'EM', 'ES', 'ES', 'ES', 'ES', 
-    		 'ES', 'ES', 'PG', 'PG', 'PG'
-    ];
-    resposta = 'EM';
+  dados = [
+  		 'EF', 'EF', 'EF', 'EM', 'EM',
+  		 'EM', 'EM', 'EM', 'EM', 'EM',
+  		 'EM', 'ES', 'ES', 'ES', 'ES',
+  		 'ES', 'ES', 'PG', 'PG', 'PG'
+  ];
+  resposta = 'EM';
 	array = modaQualitativa.init(dados);
-	expect(array).toBe(resposta);    
+	expect(array).toBe(resposta);
 });
