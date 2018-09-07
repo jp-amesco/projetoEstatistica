e2049dec5d458cd1d17d3a5afaca7405026d14e7
João <joaopauloamesco@gmail.com>
2018-09-06 21:36:06 -0300

    Alterando arquivo app.js
    -comentanto as linhas que chamam funções que ainda não estão prontas

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 const recebeDados = require('./dados/entradaDados.js');
 const identificaVariavel = require('./dados/identificaVariavel.js');
-const mensagemErro = require('./erros/mensagemErroValidacaoDados.js');
+//const mensagemErro = require('./erros/mensagemErroValidacaoDados.js');
 
 
 document.querySelector('.btn').addEventListener('click', function(){
@@ -9,25 +9,25 @@ document.querySelector('.btn').addEventListener('click', function(){
   const respostaValidadeDados = recebeDados.init(inputDados);
   let variavel;
   if (respostaValidadeDados == 1 || respostaValidadeDados == 2) {
-    mensagemErro.init(respostaValidadeDados);
+    //mensagemErro.init(respostaValidadeDados);
   }else{
-    variavel = identificaVariavel.init(respostaValidadeDados);
+    //variavel = identificaVariavel.init(respostaValidadeDados);
   }
   respostaValidadeDados.sort();
   if (variavel == 'pergunta') {
-    variavel = geraModalPergunta.init();
+    //variavel = geraModalPergunta.init();
   }
 
   if (variavel == 'qualitativa') {
-    moda = modaQualitativa.init(respostaValidadeDados);
-    mediana = medianaQualitativa.init(respostaValidadeDados);
+    /*moda = modaQualitativa.init(respostaValidadeDados);
+    mediana = medianaQualitativa.init(respostaValidadeDados);*/
   }else if(variavel == 'discreta') {
-    moda = modaDiscreta.init(respostaValidadeDados);
+    /*moda = modaDiscreta.init(respostaValidadeDados);
     media = mediaDiscreta.init(respostaValidadeDados);
-    mediana = medianaDiscreta.init(respostaValidadeDados);
+    mediana = medianaDiscreta.init(respostaValidadeDados);*/
   }else if(variavel == 'continua'){
-    moda = modaDiscreta.init(respostaValidadeDados);
+    /*moda = modaDiscreta.init(respostaValidadeDados);
     media = mediaDiscreta.init(respostaValidadeDados);
-    mediana = medianaDiscreta.init(respostaValidadeDados);
+    mediana = medianaDiscreta.init(respostaValidadeDados);*/
   }
 });
