205da3d0b3ed6a13c6a607eef75edd98fa1cb5cb
joao <joaopauloamesco@gmail.com>
2018-09-08 01:44:12 +0000

    criado teste para função que calcula a media da variavel quantitativa contiua

---
+++ testes/media/testeMediaQuantitativaContinua.js
@@ -0,0 +1,10 @@
+const modaQuantitativaContinua = require('../../js/media/mediaQuantitativaContinua.js');
+
+it('espera-se que a função retorne a media da variavel quantitativa continua (MEDIA_ CONTINUA)', function(){
+  dados = [
+    47, 50, 55, 58, 61, 61, 61, 65, 65, 65, 66, 66, 67, 67, 68,
+    68, 69, 71, 71, 71, 72, 72, 73, 76, 78, 80, 82, 90, 98, 100
+  ];
+  response = modaQuantitativaContinua.init(dados);
+  expect(response).toBe(69.9);
+});
