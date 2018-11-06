d21172a85b9711cd2f4cf036aaae09ec01b78b47
Unknown <rafanonato_cardoso@hotmail.com>
2018-11-06 02:06:12 -0200

    Check the default possibilities before calculating

--- js/probabilidade/binomial.js
+++ js/probabilidade/binomial.js
@@ -18,21 +18,27 @@ function combinatoria(x,y){
 	let denominador_1 = y;
 	let denominador_2 = x - y;
 	let denominador = 0;
-	for(let i = (x-1); i>0; i--){
-		numerador = numerador * i;
+	if(y == 0 || y == x){
+		return 1;
+	}else if(y == 1){
+		return x;
+	}else{
+		for(let i = (x-1); i>0; i--){
+			numerador = numerador * i;
+		}
+
+		for(let i = (y-1); i>0;i--){
+			denominador_1 = denominador_1 * i;
+		}
+
+		for(let i = ((x-y)-1); i>0;i--){
+			denominador_2 = denominador_2 * i;
+		}
+
+		denominador = denominador_1 * denominador_2
+
+		return numerador / denominador;
 	}
-
-	for(let i = (y-1); i>0;i--){
-		denominador_1 = denominador_1 * i;
-	}
-
-	for(let i = ((x-y)-1); i>0;i--){
-		denominador_2 = denominador_2 * i;
-	}
-
-	denominador = denominador_1 * denominador_2
-
-	return numerador / denominador;
 
 }
 
